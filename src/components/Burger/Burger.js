f07abf522e351073394410d1cdde84ace0ff3a32
LaupWing <loc_strict@hotmail.com>
2019-12-16 20:09:39 +0100

    added buildcontrols js and css

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import classes from './Burger.module.css'
+import BurgerIngredient from './BurgerIngredients/BurgerIngredient'
 
 
 const burger = (props)=>{
+    let transformedIngredient = Object.keys(props.ingredients)
+        .map(key=>{
+            return [...Array(props.ingredients[key])].map((_,i)=>{ // this is javascript method which creates an array with empty spaces Array(3) creates an array with 3 empty spaces
+                return <BurgerIngredient key={key+i} type={key}/>
+            })
+        })
+        .reduce((arr,el)=>{
+            return arr.concat(el) 
+        }, []) // Flatten the array 
+    if(transformedIngredient.length === 0){
+        transformedIngredient = <p>Please start adding ingredients</p>
+    }
     return (
-        <div></div>
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top"/>
+            {transformedIngredient}
+            <BurgerIngredient type="bread-bottom"/>
+        </div>
     )
 }
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,20 @@
 import React, {Component} from 'react'
 import Aux from '../../hoc/Auxiliry'
+import Burger from  '../../components/Burger/Burger'
+
 class BurgerBuilder extends Component{
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    }
     render(){
         return(
             <Aux>
-                <div>Burger</div>
-                <div>Build Controls</div>
+                <Burger ingredients={this.state.ingredients}/>
             </Aux>
         )
     }
