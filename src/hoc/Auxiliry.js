4e1e7a42a16f88ad9e60daeb0d19d5f16cd3ecb9
LaupWing <loc_strict@hotmail.com>
2019-12-16 18:41:23 +0100

    base setup

---
+++ src/hoc/Auxiliry.js
@@ -0,0 +1,5 @@
+const aux = (props) => props.children
+// This is wrapper component which allows us to return adjecent elements
+// Usually in react you can only return one parent element
+// But through this little hack its possible to returnn multiple elements
+export default aux
