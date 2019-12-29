1062bdf486a29cadfbb26f86abcc8d4b43609c56
LaupWing <loc_strict@hotmail.com>
2019-12-29 16:11:10 +0100

    added order container

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -84,32 +84,12 @@ class BurgerBuilder extends Component{
         // this.setState({
         //     loading: true
         // })
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice, // recacluclate the price on the server because the user can change it on the client side
-        //     customer:{
-        //         name: 'Laup Wing',
-        //         address:{
-        //             street: 'Hello',
-        //             zipCode: '1650JA',
-        //             country: 'Holland'
-        //         },
-        //         email: 'pinda@hotmail.com'
-        //     },
-        //     deliveryMethod: 'fasteset'
-        // }
-        // axios.post('/orders.json', order)
-        //     .then(res=>{
-        //         this.setState({loading:false, purchasing: false})
-        //     })
-        //     .catch(e=>{
-        //         this.setState({loading:false, purchasing: false})
-        //     })
         const queryParams = []
         for(let i in this.state.ingredients){
             // encodeURIComponent is to convert a string to a covertable url string
             queryParams.push(encodeURIComponent(i) +  '=' + encodeURIComponent(this.state.ingredients[i]))
         }
+        queryParams.push('price='+this.state.totalPrice)
         const queryString = queryParams.join('&')
         this.props.history.push({
             pathname: '/checkout',
