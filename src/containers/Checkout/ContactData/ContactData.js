acd4f2bdc87874949ed4e18ef2d224aa848c7a51
LaupWing <loc_strict@hotmail.com>
2020-01-01 19:35:24 +0100

    added validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -14,7 +14,11 @@ class ContactData extends Component{
                     type: 'text',
                     placeholder: 'Your name'
                 },
-                value: ''
+                value: '',
+                validation:{
+                    required: true
+                },
+                valid: false
             },
             street: {
                 elementType: 'input',
@@ -22,7 +26,11 @@ class ContactData extends Component{
                     type: 'text',
                     placeholder: 'Street'
                 },
-                value: ''
+                value: '',
+                validation:{
+                    required: true
+                },
+                valid: false
             },
             zipCode: {
                 elementType: 'input',
@@ -30,7 +38,11 @@ class ContactData extends Component{
                     type: 'text',
                     placeholder: 'Zipcode'
                 },
-                value: ''
+                value: '',
+                validation:{
+                    required: true
+                },
+                valid: false
             },
             country: {
                 elementType: 'input',
@@ -38,7 +50,11 @@ class ContactData extends Component{
                     type: 'text',
                     placeholder: 'Country'
                 },
-                value: ''
+                value: '',
+                validation:{
+                    required: true
+                },
+                valid: false
             },
             email: {
                 elementType: 'input',
@@ -46,7 +62,11 @@ class ContactData extends Component{
                     type: 'email',
                     placeholder: 'example@hotmail.com'
                 },
-                value: ''
+                value: '',
+                validation:{
+                    required: true
+                },
+                valid: false
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -62,7 +82,7 @@ class ContactData extends Component{
                         },
                     ]
                 },
-                value: 'fastest'
+                value: 'fastest',
             },
         },
         loading: false
@@ -89,6 +109,15 @@ class ContactData extends Component{
             })
     }
 
+    checkValidity(value, rules){
+        let isValid = false
+        if(rules.required){
+            isValid = value.trim() !== ''
+        }
+
+        return isValid
+    }
+
     inputChangedHandler = (event, inputId)=>{
         const updatedOrderForm = {
             ...this.state.orderForm
@@ -98,6 +127,8 @@ class ContactData extends Component{
         }
 
         updatedFormEl.value = event.target.value
+        updatedFormEl.valid = this.checkValidity(updatedFormEl.value, updatedFormEl.validation)
+        console.log(updatedFormEl)
         updatedOrderForm[inputId] = updatedFormEl
         this.setState({
             orderForm: updatedOrderForm
