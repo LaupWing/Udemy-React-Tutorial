fbe727a822b787e1fc265eb54a6bbbbca3679704
LaupWing <loc_strict@hotmail.com>
2020-01-19 15:54:32 +0100

    logout auth in redux

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,20 @@ export const authFail = (error)=>{
     }
 }
 
+export const logout = ()=>{
+    return{
+        type: actionTypes.AUTH_LOGOUT
+    }
+}
+
+export const checkAuthTimeout = (exparationTime) =>{
+    return dispatch =>{
+        setTimeout(()=>{
+            dispatch(logout())
+        },exparationTime*1000)
+    }
+}
+
 export const auth = (email,password, isSignup)=>{
     return dispatch =>{
         dispatch(authStart())
@@ -42,6 +56,7 @@ export const auth = (email,password, isSignup)=>{
             .then(response=>{
                 console.log(response)
                 dispatch(authSucces(response.data.idToken, response.data.localId))
+                dispatch(checkAuthTimeout(response.data.expiresIn))
             })
             .catch(err=>{
                 console.log(err)
