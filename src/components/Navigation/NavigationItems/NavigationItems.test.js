c9d580333fca58ac433686a5eb99a8899a9def0f
LaupWing <loc_strict@hotmail.com>
2020-01-22 18:26:58 +0100

    added tests

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -7,8 +7,20 @@ import React from 'react'
 configure({adapter: new Adapter()})
 
 describe('<NavigationItems/>',()=>{
+    let wrapper
+    beforeEach(()=>{
+        wrapper = shallow(<NavItems/>)
+    })
     it('should render two <NavigationItem/> elements when not auth',()=>{
-        const wrapper = shallow(<NavItems/>)
         expect(wrapper.find(NavItem)).toHaveLength(2)
+    })
+    it('should render three <NavigationItem/> elements when auth',()=>{
+        // wrapper = shallow(<NavItems isAuth/>)
+        wrapper.setProps({isAuth: true})
+        expect(wrapper.find(NavItem)).toHaveLength(3)
+    })
+    it('should render Logout when auth',()=>{
+        wrapper.setProps({isAuth: true})
+        expect(wrapper.contains(<NavItem link="/logout">Logout</NavItem>)).toEqual(true)
     })
 } )
